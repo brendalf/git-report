14ebd8b4e498c4026759d7964a5285275c4a7ac8
James Boland <jamespatrickboland@gmail.com>
2023-02-17 17:06:40 +0000

    Added a function that shows us how many commits a file has featured in

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,8 +1,9 @@
 use std::process::Command;
+use std::collections::HashMap;
 
 #[macro_export]
-macro_rules! run_cmd {
-    ($prog:expr, $($x:tt)*) =>  {{
+macro_rules! run_git {
+    ($prog:expr; $($x:tt),*)  =>  {{
         let mut cmd = Command::new($prog);
         $(cmd.arg($x);)*
         let cmd_output = cmd.output().expect("Invalid command");
@@ -12,7 +13,15 @@ macro_rules! run_cmd {
 
 
 pub fn get_tracked_files() -> Vec<String> {
-    let file_string: String = run_cmd!("git", "ls-files");
+    let file_string: String = run_git!("git"; "ls-files");
     return file_string.lines().map(|x| x.to_string()).collect();
 }
 
+pub fn return_file_activity(files: &Vec<String>) -> HashMap<String, u32> {
+    let mut output: HashMap<String,u32> = HashMap::new();
+    for file in files {
+        let commit_output: String = run_git!("git"; "rev-list", "--count", "HEAD", "--", file);
+        output.insert(file.to_owned(), commit_output.trim().parse::<u32>().expect("Value wasn't a valid integer"));
+    }
+    return output;
+}
